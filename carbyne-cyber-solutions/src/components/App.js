7533d0562137322d758571b84c5778f816be29b5
tkyg <ttenkyong@gmail.com>
2023-09-02 18:48:20 -0400

    using styled components

---
+++ carbyne-cyber-solutions/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: salmon;
+`
+
+const App = () => {
+  return (
+    <Container>Carbyne Cyber Solutions</Container>
+  )
+}
+
+export default App
