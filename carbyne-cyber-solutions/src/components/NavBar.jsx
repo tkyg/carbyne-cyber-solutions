a2a73a35e24f5a79027022f2950e379da1745ec9
tkyg <ttenkyong@gmail.com>
2023-09-02 21:31:04 -0400

    navbar styling and logo

--- carbyne-cyber-solutions/src/components/NavBar.jsx
+++ carbyne-cyber-solutions/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import CarbyneCyberSolutionsBlack from "../public/Carbyne Cyber Solutions_Black.jpeg"
 
 const Container = styled.div`
   height: 50px;
@@ -7,36 +8,57 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: cover;
   justify-content: space-between;
 `
 
 const Left = styled.div`
-  width: 60%;
+  width: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const Right = styled.div`
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
 
-const Logo = styled.h1`
+const Logo = styled.img`
+  width: 170px;
+  height: 60px;
+  object-fit: scale-down;
+`
+
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+
+`;
+const MenuItem = styled.li`
+  margin-right: 30px;
+  font-size: 20px;
   font-weight: bold;
-  text-decoration: underline crimson;
-`
-
-const Menu = styled.ul``;
-const MenuItem = styled.li``;
+  color: gray;
+`;
 
 const NavBar = () => {
   return (
     <Container>
       <Wrapper>
-        <span>
-          <Logo>Carbyne Cyber Solutions</Logo>
-          <Menu>
+        <Left>
+          <Logo src={CarbyneCyberSolutionsBlack} alt="Carbyne Logo" />
+        </Left>
+        <Right>
+        <Menu>
             <MenuItem>Home</MenuItem>
             <MenuItem>Services</MenuItem>
-            <MenuItem>Contact</MenuItem>
             <MenuItem>Partners</MenuItem>
             <MenuItem>Leadership</MenuItem>
+            <MenuItem>Contact</MenuItem>
           </Menu>
-        </span>
-        <span>button</span>
+        </Right>
 
       </Wrapper>       
     </Container>
